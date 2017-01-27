2913776ff82e190803464b0dab65e9195518ee10
jnguyen7410 <jnguyen7410@gmail.com>
2017-01-27 03:41:43 -0800

    Finished - Section 4, Lecture 36

--- app/src/main/java/com/nguyeningcode/jnguyen7410/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/nguyeningcode/jnguyen7410/buttonclickcounter/MainActivity.java
@@ -1,13 +1,41 @@
 package com.nguyeningcode.jnguyen7410.buttonclickcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+    private int numTimesClicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numTimesClicked += 1;
+                String result = "The button got tapped " + numTimesClicked + " time";
+                if (numTimesClicked != 1) {
+                    result = "\n" + result + "s";
+                }
+                textView.append(result);
+            }
+        };
+        button.setOnClickListener(onClickListener);
     }
 }

8ee70c2b3faa87c171e8c61740f679d47310f3ee
jnguyen7410 <jnguyen7410@gmail.com>
2017-01-30 00:04:05 -0800

    Added log messages

--- app/src/main/java/com/nguyeningcode/jnguyen7410/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/nguyeningcode/jnguyen7410/buttonclickcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.nguyeningcode.jnguyen7410.buttonclickcounter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,13 +13,18 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
     private TextView textView;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: in");
+        
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         userInput = (EditText) findViewById(R.id.editText);
+        //sets the input box to empty
+        userInput.setText("");
         Button button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
         textView.setText("");
@@ -29,8 +35,17 @@ public class MainActivity extends AppCompatActivity {
                 String result = userInput.getText().toString();
                 result = result + "\n";
                 textView.append(result);
+                //clears the input box after adding it to the list
+                userInput.setText("");
             }
         };
         button.setOnClickListener(onClickListener);
+        Log.d(TAG, "onCreate: out");
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
 }
